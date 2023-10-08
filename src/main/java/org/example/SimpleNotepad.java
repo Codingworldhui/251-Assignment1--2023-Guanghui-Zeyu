bddefeca17c3af8d55bc0b4ed0c70d94d68341c3
OrangeAndCat114514 <1710756576@qq.com>
2023-10-08 17:44:14 +0800

    final version

--- src/main/java/org/example/SimpleNotepad.java
+++ src/main/java/org/example/SimpleNotepad.java
@@ -1,6 +1,9 @@
 package org.example;
 
 import javax.swing.*;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.DefaultStyledDocument;
+import javax.swing.text.rtf.RTFEditorKit;
 import java.awt.*;
 import java.awt.datatransfer.*;
 import java.awt.event.ActionEvent;
@@ -13,11 +16,53 @@ import java.awt.print.PrinterJob;
 import java.awt.print.PageFormat;
 import java.awt.print.Printable;
 import java.awt.print.PrinterException;
+import java.util.Iterator;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.common.PDRectangle;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import org.fife.ui.rsyntaxtextarea.RSyntaxTextArea;
+import org.fife.ui.rsyntaxtextarea.SyntaxConstants;
+import org.fife.ui.rtextarea.RTextScrollPane;
+import org.odftoolkit.simple.TextDocument;
+import org.odftoolkit.simple.text.Paragraph;
 
 public class SimpleNotepad extends JFrame {
+    public static void convertTextToPdf(String textContent) throws IOException {
+        JFileChooser fileChooser = new JFileChooser();
+        int result = fileChooser.showSaveDialog(null);
 
-    private JTextArea textArea;//文本区域
+        if (result == JFileChooser.APPROVE_OPTION) {
+            File selectedFile = fileChooser.getSelectedFile();
+            String outputPath = selectedFile.getAbsolutePath() + ".pdf";
+
+            PDDocument document = new PDDocument();
+            PDPage page = new PDPage(PDRectangle.A4);
+            document.addPage(page);
+
+            PDPageContentStream contentStream = new PDPageContentStream(document, page);
+            contentStream.setFont(PDType1Font.HELVETICA, 12);
+
+            contentStream.beginText();
+            contentStream.newLineAtOffset(20, 750); // 设置文本起始坐标
+            contentStream.showText(textContent);
+            contentStream.endText();
+            contentStream.close();
+
+            document.save(outputPath);
+            document.close();
+
+            JOptionPane.showMessageDialog(null, "Text successfully converted to PDF.",
+                    "Conversion Success", JOptionPane.INFORMATION_MESSAGE);
+        }
+    }
+   // private JTextArea textArea;//文本区域
+   private RSyntaxTextArea textArea;
     private JFileChooser fileChooser;
+    static JMenuItem convertPDF=new JMenuItem("convertPDF");
+    static File fileAddress;
     public static void Print (String content){
         PrinterJob printerJob = PrinterJob.getPrinterJob();
         PageFormat pageFormat = printerJob.defaultPage();
@@ -50,8 +95,12 @@ public class SimpleNotepad extends JFrame {
         setSize(800, 600);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 
-        textArea = new JTextArea();
-        JScrollPane scrollPane = new JScrollPane(textArea);
+     //   textArea = new JTextArea();
+        textArea = new RSyntaxTextArea(20, 60);
+        textArea.setSyntaxEditingStyle(SyntaxConstants.SYNTAX_STYLE_JAVA); // 设置默认语法为Java
+        textArea.setCodeFoldingEnabled(true); // 启用代码折叠功能
+        RTextScrollPane scrollPane = new RTextScrollPane(textArea);
+      //  JScrollPane scrollPane = new JScrollPane(textArea);
         add(scrollPane, BorderLayout.CENTER);
 
         JMenuBar menuBar = new JMenuBar();
@@ -75,21 +124,113 @@ public class SimpleNotepad extends JFrame {
         JMenu ManageMenu=new JMenu("Manage");
         JMenu HelpMenu=new JMenu("Help");
         fileChooser = new JFileChooser();
+
+
+        convertPDF.addActionListener(e -> {
+            try {
+                String content= textArea.getText();
+                convertTextToPdf(content);
+            } catch (IOException e1) {
+                JOptionPane.showMessageDialog(null, "Error occurred during conversion: " + e1.getMessage(),
+                        "Conversion Error", JOptionPane.ERROR_MESSAGE);
+            }
+        });
         openMenuItem.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 int returnVal = fileChooser.showOpenDialog(null);
                 if (returnVal == JFileChooser.APPROVE_OPTION) {
                     File file = fileChooser.getSelectedFile();
                     try {
-                        BufferedReader br = new BufferedReader(new FileReader(file));
-                        textArea.setText("");
-                        String line;
-                        while ((line = br.readLine()) != null) {
-                            textArea.append(line + "\n");
+//                        BufferedReader br = new BufferedReader(new FileReader(file));
+//                        textArea.setText("");
+//                        String line;
+//                        while ((line = br.readLine()) != null) {
+//                            textArea.append(line + "\n");
+//                        }
+//                        br.close();
+//                    } catch (IOException ex) {
+//                        ex.printStackTrace();
+//                    }
+//                }
+                        String fileName = file.getName();
+                        String directory = file.getPath();
+                        String[] parts = fileName.split("\\."); // 以点号 "." 分割文件名
+                        String postfix = null;
+                        if (parts.length > 1) {
+                            postfix = parts[parts.length - 1]; // 获取最后一个部分作为后缀
+                            // 打印后缀
+                            System.out.println("File postfix: " + postfix);
+                        } else {
+                            System.out.println("File has no postfix.");
                         }
-                        br.close();
-                    } catch (IOException ex) {
-                        ex.printStackTrace();
+                        String Style;
+                        switch (postfix) {
+                            case "java":
+                                Style = SyntaxConstants.SYNTAX_STYLE_JAVA;
+                                break;
+                            case "c":
+                                Style = SyntaxConstants.SYNTAX_STYLE_C;
+                                break;
+                            case "cpp":
+                                Style = SyntaxConstants.SYNTAX_STYLE_CPLUSPLUS;
+                                break;
+                            case "py":
+                                Style = SyntaxConstants.SYNTAX_STYLE_PYTHON;
+                                break;
+                            default:
+                                Style = SyntaxConstants.SYNTAX_STYLE_NONE; // default no rule
+                        }
+                        textArea.setSyntaxEditingStyle(Style);
+                        if (fileName == null || directory == null)
+                            return;
+                        textArea.setText("");
+                        fileAddress = new File(directory);
+
+                        if (fileName.endsWith(".rtf")) {
+                            String result = null;
+                            try {
+                                DefaultStyledDocument styledDoc = new DefaultStyledDocument();
+                                InputStream streamReader = new FileInputStream(fileAddress);
+                                new RTFEditorKit().read(streamReader, styledDoc, 0);
+                                //Gets byte[] in the encoded form of ISO-8859-1 and generates the string in the encoded form of GBK
+                                result = new String(styledDoc.getText(0, styledDoc.getLength()).getBytes("ISO8859-1"), "GBK");
+                                textArea.setText(result);
+                            } catch (IOException ex) {
+                                ex.printStackTrace();
+                            } catch (BadLocationException ex) {
+                                ex.printStackTrace();
+                            }
+                        } else if (fileName.endsWith(".odt")) {
+                            File openedFile = new File(directory);
+
+                            try {
+                                TextDocument textDocument = TextDocument.loadDocument(openedFile);
+
+                                textArea.setText("");
+
+                                // Iterate through paragraphs and append to the text area
+                                for (Iterator<Paragraph> it = textDocument.getParagraphIterator(); it.hasNext(); ) {
+                                    Paragraph paragraph = it.next();
+                                    textArea.append(paragraph.getTextContent() + "\n");
+                                }
+                            } catch (Exception exception) {
+                                throw new RuntimeException("Failed to open the file");
+                            }
+                        } else {
+                            try {
+                                BufferedReader br = new BufferedReader(new FileReader(file));
+                                textArea.setText("");
+                                String line;
+                                while ((line = br.readLine()) != null) {
+                                    textArea.append(line + "\n");
+                                }
+                                br.close();
+                            } catch (IOException ex) {
+                                ex.printStackTrace();
+                            }
+                        }
+                    } finally {
+                        return;
                     }
                 }
             }
@@ -290,7 +431,7 @@ public class SimpleNotepad extends JFrame {
 
         ManageMenu.add(TimeAndData);
         ManageMenu.add(PrintMenuItem);
-
+        ManageMenu.add(convertPDF);
         fileMenu.add(newMenuItem);
         SearchMenu.add(searchMenuItem);
         menuBar.add(fileMenu);
